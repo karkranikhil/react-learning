89ae44cdcf8aa0b497b05a0227e19fffb2e396e3
karkran <nikhil.karkra@anz.com>
2018-05-24 20:31:30 +1000

    creation of the ShowCard component and reusing it inside search component

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import preload from '../data.json'
-
+import ShowCard from './ShowCard'
 const Search =()=>(
   <div className='search'>
     {/* <pre><code>{JSON.stringify(preload, null, 4)}</code></pre> */}
     {preload.shows.map((show)=>(
-      <h3>{show.title}</h3>
+      <ShowCard show = {show}/>
     ))}
   </div>
 )
